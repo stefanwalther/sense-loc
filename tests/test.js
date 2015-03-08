b3832eae4b2ff15f0050bc9c9d315c70775ba8a8
Stefan Walther <swr@qlik.com>
2015-03-08 20:03:01 +0100

    Add logger & v0.1.2

--- tests/test.js
+++ tests/test.js
@@ -2,13 +2,14 @@
 var chai = require( 'chai' );
 var should = chai.should();
 var extloc = require( './../lib/extloc' );
+var logger = require( './../lib/logger.js' );
 
 describe( 'extloc', function () {
 	it( 'should return a valid path', function ( done ) {
 		extloc.getPath().then( function ( data ) {
-			console.log('data', data);
+			logger.silly( 'data', data );
 			data.should.not.be.empty;
 			done();
-		})
+		} )
 	} );
 } );
