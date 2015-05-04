a9cea522182c217b0e892069e29191f63a4a8848
Stefan Walther <swr@qlik.com>
2015-05-04 02:25:43 +0200

    Minor fixes

--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,20 @@
 'use strict';
 var chai = require( 'chai' );
-var should = chai.should();
+var expect = chai.expect;
 var extloc = require( './../lib/extloc' );
 var logger = require( './../lib/logger.js' );
+var fs = require( 'fs' );
 
 describe( 'extloc', function () {
-	it( 'should return a valid path', function ( done ) {
-		extloc.getPath().then( function ( data ) {
-			logger.silly( 'data', data );
-			data.should.not.be.empty;
-			done();
-		} )
+
+	it( 'should return a valid path (using a promise)', function ( done ) {
+		extloc.getPath()
+			.then( function ( data ) {
+				logger.silly( 'data', data );
+				expect( data ).not.to.be.empty;
+				expect( fs.existsSync( data ) ).to.be.true;
+				done();
+			} )
 	} );
+
 } );
