110db3587aff22346f8e5e477b852da5432ed7f4
Stefan Walther <swr@qlik.com>
2015-07-21 15:41:59 +0200

    Initial commit for sense-loc

--- test/test.js
+++ test/test.js
@@ -1,28 +1,25 @@
 'use strict';
 var chai = require( 'chai' );
 var expect = chai.expect;
-var qlikloc = require( './../lib/qlikloc' );
+var senseLoc = require( './../lib/index.js' );
 var logger = require( './../lib/logger.js' );
 var fs = require( 'fs' );
 
-console.log( 'qlikloc', qlikloc );
-
 describe( 'extloc', function () {
 
-  it( 'contains valid methods', function ( done ) {
-    expect( qlikloc ).be.ok;
-    expect( qlikloc.getExtensionPath ).to.be.function;
-    done();
-  } );
+	it( 'contains valid methods', function ( done ) {
+		expect( senseLoc ).be.ok;
+		expect( senseLoc.getExtensionPath ).to.be.function;
+		done();
+	} );
 
-  it( 'should return a valid path (using a promise)', function ( done ) {
-    qlikloc.getExtensionPath()
-      .then( function ( data ) {
-        logger.silly( 'data', data );
-        expect( data ).not.to.be.empty;
-        expect( fs.existsSync( data ) ).to.be.true;
-        done();
-      } )
-  } );
+	it( 'should return a valid path (using a promise)', function ( done ) {
+		senseLoc.getExtensionPath( function ( err, data ) {
+			logger.silly( 'data', data );
+			expect( data ).not.to.be.empty;
+			expect( fs.existsSync( data ) ).to.be.true;
+			done();
+		} );
+	} );
 
 } );
